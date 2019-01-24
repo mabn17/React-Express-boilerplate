a705c9b1a7afa069bbd40d1d046762f3f5e6e58e
Martin Borg <martin.d@live.se>
2019-01-24 22:36:44 +0100

    added Bootstrap 4.2.1

--- src/client/index.js
+++ src/client/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/app.css';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <div className="container-fluid">
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </div>,
   document.getElementById('root')
 );
