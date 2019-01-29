a95a9f5a49412e854b6c53a957704d4262d9d728
Martin Borg <martin.d@live.se>
2019-01-29 23:28:53 +0100

    added local https

--- src/server/index.js
+++ src/server/index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const fallback = require('express-history-api-fallback');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 
 const app = express();
 const port = process.env.LINUX_PORT || 8080;
+const options = {
+  key: fs.readFileSync('./src/server/enc/server.key', 'UTF-8'),
+  cert: fs.readFileSync('./src/server/enc/server.cert', 'UTF-8')
+};
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -16,4 +23,12 @@ require('./routes')(app);
 // Redirects to ouer React main file.
 app.use(fallback('index.html', { root: 'dist' }));
 // Starts up the server
-app.listen(port, () => console.log(`Listening on port ${port}!`));
+https.createServer(options, app).listen(443);
+console.log('Listening on port 443! (HTTPS)');
+
+http.createServer(app).listen(port);
+console.log(`Listening on port ${port}!`);
+
+// https.createServer(options, app).listen(443);
+
+// app.listen(port, () => console.log(`Listening on port ${port}!`));
