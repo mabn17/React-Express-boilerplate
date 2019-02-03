88181824a172d8e4770a7d98700f750a7a2f3724
Martin Borg <martin.d@live.se>
2019-02-03 01:27:13 +0100

    build error should now be fixed

--- src/server/index.js
+++ src/server/index.js
@@ -8,6 +8,7 @@ const fs = require('fs');
 
 const app = express();
 const port = process.env.LINUX_PORT || 8080;
+const mode = process.env.NODE_ENV || 'prod';
 const options = {
   key: fs.readFileSync('./src/server/https/server.key', 'UTF-8'),
   cert: fs.readFileSync('./src/server/https/server.cert', 'UTF-8')
@@ -24,17 +25,23 @@ require('./routes')(app);
 // Redirects to ouer React main file.
 app.use(fallback('index.html', { root: 'dist' }));
 
-if (process.env.NODE_ENV === 'test') {
-  module.exports = app.listen(8081, () => console.log(`Listening on port ${port}!`));
-} else {
-  // Only for production mode on local machine to test http/https.
-  // You probably want something closer to the 'test' case as default
-  // if nginx is used as a server.
-  https.createServer(options, app).listen(443);
-  console.log('Listening on port 443! (HTTPS)');
+switch (mode) {
+  case 'test':
+    // Runs on port 8081 so the cli_test dosent interfear with anything esle.
+    module.exports = app.listen(8081, () => console.log(`Listening on port ${port}!`));
+    break;
+  case 'dev':
+    // Frontend litsents to port 3000 (see webpack settings) and backend litsens to the defaul port.
+    app.listen(port, () => console.log(`Listening on port ${port}!`));
+    break;
+  default:
+    // Only for production mode on local machine to test http/https.
+    // You probably want something closer to the 'test' case as default
+    // if nginx is used as a server.
+    https.createServer(options, app).listen(443);
+    console.log('Listening on port 443! (HTTPS)');
 
-  http.createServer(app).listen(port);
-  console.log(`Listening on port ${port}! (HTTP)`);
-
-  // app.listen(port, () => console.log(`Listening on port ${port}!`));
+    http.createServer(app).listen(port);
+    console.log(`Listening on port ${port}! (HTTP)`);
+    break;
 }
